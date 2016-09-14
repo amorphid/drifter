978cea52450c917b6c7017aa086693f19dff9b3d
Michael Pope <mpope.cr@gmail.com>
2016-09-13 17:37:27 -0700

    Update elasticsearch package && make box attach executable

--- lib/package/install/elasticsearch.sh
+++ lib/package/install/elasticsearch.sh
@@ -6,12 +6,15 @@ wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 # enable apt-get installs from https debian repos
 apt-get install apt-transport-https
 
-# Save the repository definition to /etc/apt/sources.list.d/elasticsearch-2.x.list:
+# save the repository definition to /etc/apt/sources.list.d/elasticsearch-2.x.list:
 echo "deb https://packages.elastic.co/elasticsearch/2.x/debian stable main" | tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
 
-# Run apt-get update and the repository is ready for use
+# run apt-get update and the repository is ready for use
 apt-get update && apt-get install elasticsearch
 
-# Configure Elasticsearch to automatically start during bootup via Systemd
+# configure Elasticsearch to automatically start during bootup via Systemd
 /bin/systemctl daemon-reload
 /bin/systemctl enable elasticsearch.service
+
+# start Elasticsearch
+/bin/systemctl start elasticsearch.service
